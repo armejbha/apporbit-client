716a9a080fc5847cc78edb6957c7ed5ec53ae5fe
armejbha <abdurrahim181878@gmil.com>
2025-07-12 03:33:00 +0600

    project initialize

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,35 @@
+import { Children, Component } from "react";
+import { createBrowserRouter } from "react-router";
+import RootLayouts from "../Layouts/RootLayouts";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home/Home";
+import LogIn from "../Pages/LogIn/LogIn";
+import SignUp from "../Pages/SignUp/SignUp";
+
+
+const router=createBrowserRouter=([
+    {
+        path:"/",
+        element:<RootLayouts/>,
+        errorElement:<ErrorPage/>,
+        Children:[
+            {
+                index:true,
+                element:<Home/>
+            }
+        ]
+    },
+    {
+        path:"login",
+        element:<LogIn/>
+    },
+    {
+        path:"signup",
+        element:<SignUp/>
+    },
+    {
+        path:"*",
+        element:<ErrorPage/>
+    }
+
+])
